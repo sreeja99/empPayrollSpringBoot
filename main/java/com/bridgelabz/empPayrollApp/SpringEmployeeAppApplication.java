b6e129f67686321dbdfb7af7c14861ca48437d8f
sreeja99 <srijagodishala@gmail.com>
2020-11-26 19:45:41 +0530

    Added:Created A New Spring Boot Project

---
+++ main/java/com/bridgelabz/empPayrollApp/SpringEmployeeAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.empPayrollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeeAppApplication.class, args);
+	}
+
+}
---
+++ test/java/com/bridgelabz/empPayrollApp/SpringEmployeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.empPayrollApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
